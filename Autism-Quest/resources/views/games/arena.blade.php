ca9c70c9d0ca3e9897a15527c6ffb49541231c0c
salehaahmed14 <113420201+salehaahmed14@users.noreply.github.com>
2024-01-01 00:19:08 +0500

    Improve Styling

--- Autism-Quest/resources/views/games/arena.blade.php
+++ Autism-Quest/resources/views/games/arena.blade.php
@@ -5,15 +5,21 @@
      body{
         background-image: linear-gradient(to right, #fafab5 , #c9dcf9);
      }
+     .card{
+        background-color: lightpink;
+     }
+     .btn{
+
+     }
 </style>
 <section id="main-page" class="container mt-5">
     <div class="row">
         <div class="col-md-4 mb-4">
             <div class="card h-100 d-flex flex-column">
-                <img class="card-img-top" src="{{ asset('gameImages/MathMania.png') }}" alt="Math Mania">
+                <img class="card-img-top" src="{{ asset('gameImages/MathMania.png') }}" alt="Math Mania" style="height: 73%">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">Math Mania</h5>
-                    <a href="{{ asset('games/math mania/mathmania.html') }}" class="btn btn-primary mt-auto">Play</a>
+                    <a href="{{ asset('games/math mania/mathmania.html') }}" class="btn btn-outline-danger mt-auto">Play</a>
                 </div>
             </div>
         </div>
@@ -23,7 +29,7 @@
                 <img class="card-img-top" src="{{ asset('gameImages/bubblePop.png') }}" alt="Bubble Pop">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">Bubble Pop</h5>
-                    <a href="{{ asset('games/bubble pop/bubblepop.html') }}" class="btn btn-primary mt-auto">Play</a>
+                    <a href="{{ asset('games/bubble pop/bubblepop.html') }}" class="btn btn-outline-danger mt-auto">Play</a>
                 </div>
             </div>
         </div>
@@ -33,7 +39,7 @@
                 <img class="card-img-top" src="{{ asset('gameImages/EmoExplosion.png') }}" alt="Num Bubble">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">Emo Explosion</h5>
-                    <a href="{{ asset('games/EmoExplosion/emoexplosion.html') }}" class="btn btn-primary mt-auto">Play</a>
+                    <a href="{{ asset('games/EmoExplosion/emoexplosion.html') }}" class="btn btn-outline-danger mt-auto">Play</a>
                 </div>
             </div>
         </div>
